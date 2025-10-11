534060027f000a0d7c32b99ba40a2332dbeb577d
aniket44788 <darkpanda44788@gmail.com>
2025-10-12 01:24:31 +0530

    url

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 8999;
 
 app.use(
   cors({
-    origin: process.env.ORIGIN_URL,
+    origin: "https://mahakal-project-frontend.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
