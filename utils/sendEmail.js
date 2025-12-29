70d52a7aaf9147e6967d0e440a2465b6d4037d11
Aniket5353 <Aniket@acoreithub.com>
2025-12-29 11:23:55 +0530

    email

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,32 +1,15 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
 
-export const sendEmail = async (to, subject, text) => {
-  try {
-    console.log("📧 Sending email to:", to);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
-    const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, 
-      auth: {
-        user: process.env.EMAIL_USER, 
-        pass: process.env.EMAIL_PASS, 
-      },
-    });
-
-    await transporter.sendMail({
-      from: `"Mahakal" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      text,
-    });
-
-    console.log("✅ Email sent successfully");
-  } catch (error) {
-    console.error("❌ EMAIL SEND ERROR:", error);
-    throw error;
-  }
+export const sendEmail = async (to, subject, html) => {
+  await resend.emails.send({
+    from: "Mahakal <onboarding@resend.dev>",
+    to,
+    subject,
+    html,
+  });
 };
